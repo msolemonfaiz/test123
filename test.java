a10814d652da5a4709a8acaebb7d0369d8fab2db
msolemonfaiz <36687764+msolemonfaiz@users.noreply.github.com>
2018-02-21 00:25:40 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,10 @@
-test.java
+public class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println("Hello, my name is Noah!");
+        System.out.println("I will be your virtual assistant!");
+        System.out.println("Please, be my guest");
+        //System.out.println("this won't be printed!")
+        //this is a comment
+        System.exit(0); //another comment
+    }
+}
